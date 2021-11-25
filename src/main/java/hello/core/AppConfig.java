9602b2bcf856f0b70d6a2ab5065856b11df49764
This2sho <zx00018@naver.com>
2021-11-25 19:28:39 +0900

    Refactor: AppConfig 중복 제거 및 역할에 따른 구현

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DisCountpolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,10 +11,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
     // 생성자 주입 방식
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DisCountpolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
