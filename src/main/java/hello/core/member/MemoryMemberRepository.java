65e60ef8b9905f598159ff890d84f07eacb9a079
This2sho <zx00018@naver.com>
2021-11-29 20:58:15 +0900

    Refactor: Bean 자동등록을 위한 어노테이션 추가
    자동등록을 위해 class에 @Componenet 추가 및 생성자에 @Autowired 붙여서 의존관계 주입

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에서 Concurrent HashMap사용해야 동시성 이슈가 없음!
