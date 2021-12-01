2591cea9f3f07b57071b26a561a9f84c23e05be1
This2sho <zx00018@naver.com>
2021-12-01 20:47:00 +0900

    Test: 같은 종류의 할인정책을 갖는 빈 2개다 사용하는 테스트

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,51 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DisCountpolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    static class DiscountService{
+        private final Map<String, DisCountpolicy> policyMap;
+        private final List<DisCountpolicy> policies;
+
+        @Autowired
+        DiscountService(Map<String, DisCountpolicy> policyMap, List<DisCountpolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DisCountpolicy disCountpolicy = policyMap.get(discountCode);
+            return disCountpolicy.discount(member, price);
+        }
+    }
+}
