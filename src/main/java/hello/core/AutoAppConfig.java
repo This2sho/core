6bc69693311b4779c9a6e476e734c3a8bde45299
This2sho <zx00018@naver.com>
2021-12-01 20:45:58 +0900

    Chore: 사용하지않는 코드 주석처리 및 Filter 어노테이션 static import

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,15 +7,24 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import static org.springframework.context.annotation.ComponentScan.*;
+
+//@Configuration
+//@ComponentScan(// 어노테이션붙은 컴포넌트를 찾아서 자동으로 등록해
+//        basePackages = "hello.core.member", // 베이스 패키지 시작 위치 설정
+//        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지를 탐색 시작 위치로 지정 (default는 ComponentScan 쓰여진 package를 시작 위치로)
+//        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 뺄 컴포넌트 추가
+//)
+//public class AutoAppConfig {
+////    @Bean(name = "memoryMemberRepository")
+////    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////    }
+//}
+
 @Configuration
-@ComponentScan(// 어노테이션붙은 컴포넌트를 찾아서 자동으로 등록해
-        basePackages = "hello.core.member", // 베이스 패키지 시작 위치 설정
-        basePackageClasses = AutoAppConfig.class, // 지정한 클래스의 패키지를 탐색 시작 위치로 지정 (default는 ComponentScan 쓰여진 package를 시작 위치로)
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 뺄 컴포넌트 추가
-)
+@ComponentScan(
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
+                Configuration.class))
 public class AutoAppConfig {
-//    @Bean(name = "memoryMemberRepository")
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
 }
