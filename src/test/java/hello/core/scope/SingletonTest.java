e9a2cdc4dd55419ab57d069e9926d7dbb5bb9ad6
This2sho <zx00018@naver.com>
2021-12-02 18:18:41 +0900

    Test: Singleton 빈 스코프를 알아보는 테스트

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,43 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        System.out.println("find singletonBean1");
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        System.out.println("find singletonBean2");
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton") // 원래 디폴트이나 공부용으로 적음
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("SingletonBean.destory");
+        }
+    }
+
+}
