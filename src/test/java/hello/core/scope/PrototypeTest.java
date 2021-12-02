7f4021ffeb35c40cd5a893b89d5ee15f25e68a0e
This2sho <zx00018@naver.com>
2021-12-02 18:18:50 +0900

    Test: Prototype 빈 스코프를 알아보는 테스트

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destory();
+        prototypeBean2.destory();
+        ac.close();
+    }
+
+    @Scope("prototype") // Component 어노테이션이 없는 이유는 ac에 이미 PrototypeBean.class를 지정해줘서 component처럼 동작하게 되있음
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("PrototypeBean.destory");
+        }
+    }
+
+}
